b39f0d2609fdae1c8336a5016ddf9bdf3f30c471
ajrp <ajrp@tec.dk>
2024-04-01 18:25:12 +0200

    ..

--- LegionsTuenBased35_2/ArmyScene/Army/ArmyScene.gd
+++ LegionsTuenBased35_2/ArmyScene/Army/ArmyScene.gd
@@ -2,10 +2,13 @@ extends Area2D
 
 class_name ArmyScene
 
-onready var army_sprite : Sprite	= $ArmySprite
-onready var move_area : RangeScene	= $Range
-onready var target_sprite :Sprite	= $TargetSprite
-onready var label : Label			= $Label
+export var army_color	= Color.chartreuse
+export var team_id		= 0
+
+onready var army_sprite : Sprite		= $ArmySprite
+onready var range_scene : RangeScene	= $Range
+onready var target_sprite :Sprite		= $TargetSprite
+onready var label : Label				= $Label
 
 var selected : bool			= false
 var army_size : int			= 16
@@ -15,6 +18,8 @@ func _ready():
 	_set_selection(false)
 	_set_as_target(false)
 	_set_destination(global_position)
+	army_sprite.self_modulate	= army_color
+	range_scene.team_id 		= self.team_id
 	pass
 
 func _process(delta):
@@ -24,21 +29,39 @@ func _process(delta):
 
 func _is_army_position(global_selected_position : Vector2):
 	var v = global_selected_position - global_position
-	if v.length() < army_size: return true
+	if v.length() < army_size: 
+		return true
 	pass
 	
 func _set_selection(select : bool):
-	move_area.visible 		= select
 	selected				= select
+	range_scene._show_range(select)
 	pass
 			
+
 func _set_as_target(is_target : bool):
 	target_sprite.visible = is_target
 	pass
 
+
 func _set_destination(new_destination: Vector2):
-	var valid_move =  move_area._is_inside(new_destination)
+	var valid_move =  range_scene._is_inside(new_destination)
 	if valid_move:
 		destination = new_destination;
 	return valid_move
+	pass
+
+
+func get_class():
+	return "ArmyScene"
+	
+	
+func _on_Army_area_entered(area):
+	if area.get_class() == "ArmyScene" and area.team_id != self.team_id:
+		destination = global_position
+		pass
+	pass
+
+
+func _on_Army_area_exited(area):
 	pass
--- LegionsTuenBased35_2/ArmyScene/Range/RangeScene.gd
+++ LegionsTuenBased35_2/ArmyScene/Range/RangeScene.gd
@@ -2,9 +2,18 @@ extends Area2D
 
 class_name RangeScene
 
-var radius : int 	= 126
+export var team_id = 0;
 
-func _ready():
+var radius : int 	= 126
+var enemies_inside	= []
+
+func _process(delta):
+	update_label()
+	pass
+
+func _show_range(showit:bool):
+	visible = showit
+	_show_targets(showit)
 	pass
 
 func _is_inside(position:Vector2):
@@ -12,3 +21,25 @@ func _is_inside(position:Vector2):
 	if v.length() < radius:
 		return true 
 	pass
+
+func _on_Range_area_entered(area):
+	if area.get_class() == "ArmyScene" and area.team_id != self.team_id:
+		enemies_inside.append(area)
+	_show_targets(visible)	
+	pass
+
+func _on_Range_area_exited(area):
+	if area.get_class() == "ArmyScene" and area.team_id != self.team_id:
+		area._set_as_target(false)
+		enemies_inside.erase(area)
+	pass
+
+func update_label():
+	$Label.text = "team:" + str(team_id) + "\n enemies in: " + str(enemies_inside.size())
+	pass
+	
+func _show_targets(showtarget):
+	for a in enemies_inside:
+		a._set_as_target(showtarget)
+	pass
+
