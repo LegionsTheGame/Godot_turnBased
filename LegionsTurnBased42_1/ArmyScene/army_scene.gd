307c9deef546dd100c5ec8c33352c460b00fad92
ajrp <ajrp@tec.dk>
2024-04-09 09:21:38 +0200

    ---

--- LegionsTurnBased42_1/ArmyScene/army_scene.gd
+++ LegionsTurnBased42_1/ArmyScene/army_scene.gd
@@ -1,7 +1,12 @@
 extends Area2D
 class_name Army
 
+@onready var army_sprite : Sprite2D		= $ArmySprite
+@onready var selected_sprite :Sprite2D	= $ArmySelectedSprite 
+
 var destination : Vector2
+
+signal army_selected(army)
 
 func _ready():	
 	destination = global_position
@@ -9,15 +14,19 @@ func _ready():
 
 func _process(delta):
 	var m = (destination - global_position)
-	if m.length() > 2:
-		global_position += m.normalized()
+	if m.length() > 2: global_position += m.normalized()
 	pass
 
-func _set_destination(d:Vector2):
+func _set_destination(d:Vector2): 
 	destination = d
 	pass
 
+func _set_selected(s:bool):
+	selected_sprite.visible = s
+	pass
 
-func _on_mouse_entered():
-	print("mouse entered army")
+func _on_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("mouse_left"):
+		print("army_clicked");
+		emit_signal("army_selected",self)
 	pass
